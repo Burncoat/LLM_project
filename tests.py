6e17a92ec24096b4b01611d31075ba1b6b824a8e
Burncoat <htvick@gmail.com>
2025-06-12 12:56:44 -0400

    Added a bunch of stuff. Ch1-3 of the course on boot.dev

---
+++ tests.py
@@ -0,0 +1,28 @@
+import unittest
+from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
+from functions.write_file import write_file
+from functions.run_python import run_python_file
+
+def print_result(result):
+    print("Result for current directory")
+    print(result)
+    print("")
+
+def test():
+    result = run_python_file("calculator", "main.py")
+    print(result)
+
+    result = run_python_file("calculator", "tests.py")
+    print(result)
+
+    result = run_python_file("calculator", "../main.py")
+    print(result)
+
+    result = run_python_file("calculator", "nonexistent.py")
+    print(result)
+
+if __name__ == "__main__":
+    test()
+
+
